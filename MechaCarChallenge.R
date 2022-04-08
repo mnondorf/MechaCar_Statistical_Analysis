88cd33c0e616d6573eed6d73c70c2902d3e23ebd
Melissa <melisochka926@gmail.com>
2022-04-08 12:33:34 -0500

    Module Challenge Deliverable 2 Upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -2,3 +2,7 @@ library(dplyr)
 MechaCars_df <- read.csv(file = "MechaCar_mpg.csv", header = TRUE)
 lr_model =lm(mpg~vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, MechaCars_df)
 summary(lr_model)
+
+suspension <- read.csv(file ="Suspension_coil.csv", header = TRUE)
+total_summary <- suspension %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+lot_summary <- suspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
