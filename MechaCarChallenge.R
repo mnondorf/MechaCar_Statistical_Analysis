5498b6c83292e0070790b3f6829c89fd61e2f1e1
Melissa <melisochka926@gmail.com>
2022-04-09 13:31:21 -0500

    Module Challenge Deliverable 3 Upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -6,3 +6,13 @@ summary(lr_model)
 suspension <- read.csv(file ="Suspension_coil.csv", header = TRUE)
 total_summary <- suspension %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
 lot_summary <- suspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+
+lot1 <- subset(suspension, Manufacturing_Lot == "Lot1")
+lot2 <- subset(suspension, Manufacturing_Lot == "Lot2")
+lot3 <- subset(suspension, Manufacturing_Lot == "Lot3")
+
+t.test(suspension$PSI, mu=1500)
+
+t.test(lot1$PSI, mu=1500)
+t.test(lot2$PSI, mu=1500)
+t.test(lot3$PSI, mu=1500)
